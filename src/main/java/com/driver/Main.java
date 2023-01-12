90b9efb1cff98688b8af31caa4bf4632d4b6aedb
shefankhan <shefanofficial@gmail.com>
2023-01-13 01:14:43 +0530

    first change

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,31 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        int x=1;
+        int y=2;
+        int z=3;
+        double d1=1.1;
+        double d2=2.2;
 
+        Product p =new Product();
+        //p.product(x,y);
+        //p.product(x,y,z);
+       // p.product(d1,d2);
+        System.out.println(p.product(x,y));
+        System.out.println(p.product(x,y,z));
+        System.out.println(p.product(d1,d2));
+    }
+}
+
+class Product{
+    public int product(int x, int y) {
+         return x+y;
+    }
+    public int product(int x, int y, int z) {
+        return x+y+z;
+    }
+    public double product(double x, double y) {
+        return x+y;
+    }
 }
