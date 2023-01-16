90fe58ab040585be44966a9b1c23316d0aea27db
shefankhan <shefanofficial@gmail.com>
2023-01-16 19:06:27 +0530

    latest fix

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,6 +1,17 @@
 package com.driver;
 
 public class Main {
+    public static class Product{
+        public int product(int x, int y) {
+            return x*y;
+        }
+        public int product(int x, int y, int z) {
+            return x*y*z;
+        }
+        public double product(double x, double y) {
+            return x*y;
+        }
+    }
     public static void main(String[] args) {
         int x=1;
         int y=2;
@@ -18,14 +29,3 @@ public class Main {
     }
 }
 
-class Product{
-    public int product(int x, int y) {
-         return x+y;
-    }
-    public int product(int x, int y, int z) {
-        return x+y+z;
-    }
-    public double product(double x, double y) {
-        return x+y;
-    }
-}
